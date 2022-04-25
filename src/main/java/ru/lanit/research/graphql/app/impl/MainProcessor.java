b08e180846c965fc1bcaf4141e7e324176cf60b1
Ермаков Денис <DEErmakov@lan.lanit.ru>
2022-04-25 18:01:59 +0300

    Initial commit

--- src/main/java/ru/lanit/research/graphql/app/impl/MainProcessor.java
+++ src/main/java/ru/lanit/research/graphql/app/impl/MainProcessor.java
@@ -2,6 +2,7 @@ package ru.lanit.research.graphql.app.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,5 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class MainProcessor {
-
+    @Scheduled(fixedDelay = 5000)
+    public void save() {
+    }
 }
